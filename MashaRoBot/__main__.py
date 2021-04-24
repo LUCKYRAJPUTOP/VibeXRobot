a3caf7718cfede70ae0aa20946b6349df4a08f96
AlphaTp <83142843+AlphaTp@users.noreply.github.com>
2021-04-25 04:20:56 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -360,9 +360,9 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Masha's licensed under the GNU General Public License v3.0_
+                 \n_ licensed under the GNU General Public License v3.0_
                  Here is the [💾Repository](https://github.com/AlphaTp/MashaRoBot).
-                 If you have any question about Masha, let us know at @.""",
+                 If you have any question about Masha, let us know at .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
